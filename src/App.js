3b1c235613a5b59bec86458cb12d3babc85fc790
gyeolse <threewave@kakao.com>
2022-08-09 22:24:44 +0900

    ADD : Initialize front side projects

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+
+function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />}></Route>
+          {/* this is redirect option
+        <Route path="*" element={<Navigate to="/" />}></Route> */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/screens/Login.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { darkModeVar, isLoggedInVar } from "../apollo";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Container = styled.div``;
+
+function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>To dark</button>
+      <button onClick={() => darkModeVar(false)}>To light</button>
+      <button onClick={() => isLoggedInVar(true)}>Log in Now ! </button>
+    </Container>
+  );
+}
+export default Login;
